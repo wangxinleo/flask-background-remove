7b529132c8080515300ffc880299e191bcb8cd97
wangxinleo <wangxin.leo@outlook.com>
2019-08-01 16:58:44 +0800

    :heart:index首页完成，RemoveBg功能完成

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, abort, redirect, url_for, escape, session, flash
+from flask import Flask, render_template, request, abort, redirect, url_for, session
 import os
 from werkzeug.utils import secure_filename
+from removebg import RemoveBg
+import PIL.Image as Image
+import shutil
 
-
-UPLOAD_FOLDER = '%s' %os.getcwd()  # 上传路径
+APIKEY = ""
+UPLOAD_FOLDER = '%s\\uploadFile' %os.getcwd()  # 上传路径
+DOWNLOAD_FOLDER = '%s\\downloadFile' %os.getcwd() # 下载路径
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])  # 允许上传的文件类型
 
 
@@ -14,6 +18,7 @@ app.secret_key = '1111111111'  # os.urandom(16)
 
 def allowed_file(filename):  # 验证上传文件是否符合要求
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 
 @app.route('/')
@@ -47,8 +52,39 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)  # 获取上传的文件名
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  # 保存文件
-            flash('{} 上传成功'.format(filename))
-            return render_template('upload.html')  # 返回保存结果
+            # flash('{} 上传成功'.format(filename))
+            # return render_template('upload.html')  # 返回保存结果
+            rmbg = RemoveBg(APIKEY, "error.log")
+            for pic in os.listdir(UPLOAD_FOLDER):
+                tempPic = pic.rsplit('.', 1)[0]
+                url = "%s\%s" % (UPLOAD_FOLDER, pic)
+                rmbg.remove_background_from_img_file(url)
+
+                oldImg = url + "_no_bg.png"
+                newImg = "%s\%s" % (DOWNLOAD_FOLDER, tempPic+".png")
+                try:
+                    shutil.move(oldImg, newImg)
+
+                    im = Image.open(newImg)
+                    x, y = im.size
+                    p = Image.new('RGBA', im.size, (255,255,255))
+                    p.paste(im, (0, 0, x, y), im)
+                    p.save("%s\%s" % (DOWNLOAD_FOLDER, tempPic + '_white.png'))
+
+                    im = Image.open(newImg)
+                    x, y = im.size
+                    p = Image.new('RGBA', im.size, (0,0,255))
+                    p.paste(im, (0, 0, x, y), im)
+                    p.save("%s\%s" % (DOWNLOAD_FOLDER, tempPic + '_blue.png'))
+
+                    im = Image.open(newImg)
+                    x, y = im.size
+                    p = Image.new('RGBA', im.size, (255,0,0))
+                    p.paste(im, (0, 0, x, y), im)
+                    p.save("%s\%s" % (DOWNLOAD_FOLDER, tempPic + '_red.png'))
+                except FileNotFoundError:
+                    return redirect(url_for('index'))
+            return redirect(url_for('index'))
         else:
             return '非法的文件格式或操作！<a href="/upload">返回</a> '
     else:
